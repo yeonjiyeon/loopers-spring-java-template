a143f91054fee4ac1e6d58fac3e3efd9b221640b
이정배 <jeongbae1212@gmail.com>
2025-10-29 15:19:41 +0900

    round1: User가 BaseEntity 상속받도록 수정,User.id -> User.userId로 수정

--- apps/commerce-api/src/main/java/com/loopers/domain/user/User.java
+++ apps/commerce-api/src/main/java/com/loopers/domain/user/User.java
@@ -1,23 +1,33 @@
 package com.loopers.domain.user;
 
+import com.loopers.domain.BaseEntity;
 import com.loopers.support.error.CoreException;
 import com.loopers.support.error.ErrorType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
 import org.apache.commons.lang3.StringUtils;
 
-public class User {
-    private final String id;
-    private final String email;
-    private final String birthday;
+@Entity
+@Table(name = "user")
+@Getter
+public class User extends BaseEntity {
+    protected User() {
+    }
 
-    private User(String id, String email, String birthday) {
-        this.id = id;
+    private String userId;
+    private String email;
+    private String birthday;
+
+    private User(String userId, String email, String birthday) {
+        this.userId = userId;
         this.email = email;
         this.birthday = birthday;
     }
 
-    public static User create(String id, String email, String birthday) {
+    public static User create(String userId, String email, String birthday) {
         // ID 가 영문 및 숫자 10자 이내 형식에 맞지 않으면, User 객체 생성에 실패한다.
-        if (StringUtils.isBlank(id) || !id.matches("^(?=.*[a-zA-Z])(?=.*\\d)[a-zA-Z\\d]{1,10}$")) {
+        if (StringUtils.isBlank(userId) || !userId.matches("^(?=.*[a-zA-Z])(?=.*\\d)[a-zA-Z\\d]{1,10}$")) {
             throw new CoreException(ErrorType.BAD_REQUEST, "ID는 영문 및 숫자 10자 이내여야 합니다.");
         }
         // 이메일이 xx@yy.zz 형식에 맞지 않으면, User 객체 생성에 실패한다.
@@ -29,6 +39,6 @@ public class User {
             throw new CoreException(ErrorType.BAD_REQUEST, "생년월일 형식이 올바르지 않습니다.");
         }
 
-        return new User(id, email, birthday);
+        return new User(userId, email, birthday);
     }
 }
