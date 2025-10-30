3158c2bce49c3e9a6cea90cc499c54688ecb169a
adminhelper <201401339hs@gmail.com>
2025-10-31 05:07:37 +0900

    test: 포인트 도메인 단위테스트

---
+++ apps/commerce-api/src/test/java/com/loopers/domain/point/PointTest.java
@@ -0,0 +1,23 @@
+package com.loopers.domain.point;
+
+import com.loopers.support.error.CoreException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class PointTest {
+    @DisplayName("Point 단위 테스트")
+    @Nested
+    class Charge {
+        @DisplayName("0 이하의 정수로 포인트를 충전 시 실패한다.")
+        @Test
+        void throwsChargeAmountFailException_whenZeroAmountOrNegative() {
+            Point point = new Point("yh45g", 0L);
+            assertThrows(CoreException.class, () ->
+                    point.charge(0L));
+        }
+    }
+
+}
