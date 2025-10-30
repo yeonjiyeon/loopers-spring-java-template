c228ce5560e2511792405cececbb82ce8c4418a8
yeonjiyeon <dalnim95@naver.com>
2025-10-30 20:40:17 +0900

    feat: 내 정보 조회 구현 및 E2E 테스트

--- apps/commerce-api/src/main/java/com/loopers/interfaces/api/user/UserV1ApiSpec.java
+++ apps/commerce-api/src/main/java/com/loopers/interfaces/api/user/UserV1ApiSpec.java
@@ -3,6 +3,7 @@ package com.loopers.interfaces.api.user;
 import com.loopers.interfaces.api.ApiResponse;
 import com.loopers.interfaces.api.user.UserV1Dto.UserResponse;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @Tag(name = "User V1 API", description = "사용자 관련 API 입니다.")
@@ -13,5 +14,14 @@ public interface UserV1ApiSpec {
       description = "신규 사용자를 등록합니다."
   )
   ApiResponse<UserResponse> signUp(UserV1Dto.SignUpRequest request);
+
+  @Operation(
+      summary = "내 정보 조회",
+      description = "헤더의 ID로 내 정보를 조회합니다."
+  )
+  ApiResponse<UserV1Dto.UserResponse> getMyInfo(
+      @Schema(name = "USER-ID", description = "조회할 사용자 ID (헤더)")
+          String userId
+  );
 
 }
--- apps/commerce-api/src/main/java/com/loopers/interfaces/api/user/UserV1Controller.java
+++ apps/commerce-api/src/main/java/com/loopers/interfaces/api/user/UserV1Controller.java
@@ -1,9 +1,14 @@
 package com.loopers.interfaces.api.user;
 
+import com.loopers.domain.user.User;
 import com.loopers.domain.user.UserService;
 import com.loopers.interfaces.api.ApiResponse;
 import com.loopers.interfaces.api.user.UserV1Dto.UserResponse;
+import com.loopers.support.error.CoreException;
+import com.loopers.support.error.ErrorType;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +26,18 @@ public class UserV1Controller implements UserV1ApiSpec {
   public ApiResponse<UserResponse> signUp(@RequestBody UserV1Dto.SignUpRequest request) {
 
     UserResponse response = userService.signUp(request.toCommand());
+    return ApiResponse.success(response);
+  }
+
+  @GetMapping("/{userId}")
+  @Override
+  public ApiResponse<UserResponse> getMyInfo(@PathVariable(value = "userId") String userId) {
+    User user = userService.getUser(userId);
+
+    if(user == null) {
+      throw new CoreException(ErrorType.NOT_FOUND, "[userId = " + userId + "] 예시를 찾을 수 없습니다.");
+    }
+    UserResponse response = UserResponse.from(user);
     return ApiResponse.success(response);
   }
 
