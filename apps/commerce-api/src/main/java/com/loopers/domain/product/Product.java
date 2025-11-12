bdc878ee4fd41912a4db26fe25106ba5ad271cdb
yeonjiyeon <dalnim95@naver.com>
2025-11-12 19:28:15 +0900

    feat: 제품 도메인 생성 및 단위 테스트 작성

---
+++ apps/commerce-api/src/main/java/com/loopers/domain/product/Product.java
@@ -0,0 +1,48 @@
+package com.loopers.domain.product;
+
+import com.loopers.domain.BaseEntity;
+import com.loopers.support.error.CoreException;
+import com.loopers.support.error.ErrorType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "user")
+public class Product extends BaseEntity {
+
+  private Long brandId;
+  private String name;
+  private String description;
+  private long price;
+  private int stock;
+
+  public Product(Long brandId, String name, String description, long price, int stock) {
+    if (brandId == null) {
+      throw new CoreException(ErrorType.BAD_REQUEST, "상품의 브랜드를 등록해야 합니다.");
+    }
+
+    if (name == null || name.isBlank()) {
+      throw new CoreException(ErrorType.BAD_REQUEST, "상품의 이름을 등록해야 합니다.");
+    }
+
+    if (description == null || description.isBlank()) {
+      throw new CoreException(ErrorType.BAD_REQUEST, "상품의 설명을 등록해야 합니다.");
+    }
+
+    if (price < 0) {
+      throw new CoreException(ErrorType.BAD_REQUEST, "상품의 가격은 음수가 될 수 없습니다.");
+    }
+
+    if (stock < 0) {
+      throw new CoreException(ErrorType.BAD_REQUEST, "상품의 재고는 음수가 될 수 없습니다.");
+    }
+
+    this.brandId = brandId;
+    this.name = name;
+    this.description = description;
+    this.price = price;
+    this.stock = stock;
+  }
+
+
+}
