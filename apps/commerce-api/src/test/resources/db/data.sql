2a57c6356ae36bc61f65a99d3cee3a3acaa7e98d
yeonjiyeon <dalnim95@naver.com>
2025-11-27 22:51:34 +0900

    docs: 인덱스 생성 쿼리

--- apps/commerce-api/src/test/resources/db/data.sql
+++ apps/commerce-api/src/test/resources/db/data.sql
@@ -8,11 +8,32 @@ INSERT INTO brand (name, description, created_at, updated_at) VALUES
 ('Chanel', 'Luxury', NOW(), NOW()),
 ('Gucci', 'Luxury', NOW(), NOW()),
 ('Zara', 'Clothing', NOW(), NOW()),
-('Uniqlo', 'Clothing', NOW(), NOW());
+('Uniqlo', 'Clothing', NOW(), NOW()),
+('Puma', 'Sports Brand', NOW(), NOW()),
+('Reebok', 'Sports Brand', NOW(), NOW()),
+('Microsoft', 'Tech Brand', NOW(), NOW()),
+('Amazon', 'E-commerce', NOW(), NOW()),
+('Meta', 'Tech Brand', NOW(), NOW()),
+('Google', 'Tech Brand', NOW(), NOW()),
+('Lenovo', 'Electronics', NOW(), NOW()),
+('Asus', 'Electronics', NOW(), NOW()),
+('Dell', 'Electronics', NOW(), NOW()),
+('HP', 'Electronics', NOW(), NOW()),
+('Prada', 'Luxury', NOW(), NOW()),
+('Hermes', 'Luxury', NOW(), NOW()),
+('Burberry', 'Luxury', NOW(), NOW()),
+('Coach', 'Luxury', NOW(), NOW()),
+('H&M', 'Clothing', NOW(), NOW()),
+('Bershka', 'Clothing', NOW(), NOW()),
+('Pull&Bear', 'Clothing', NOW(), NOW()),
+('New Balance', 'Sports Brand', NOW(), NOW()),
+('The North Face', 'Outdoor Brand', NOW(), NOW()),
+('Columbia', 'Outdoor Brand', NOW(), NOW());
 
 DROP PROCEDURE IF EXISTS loop_insert_products;
 
 DELIMITER $$
+
 CREATE PROCEDURE loop_insert_products()
 BEGIN
     DECLARE i INT DEFAULT 1;
@@ -24,19 +45,19 @@ BEGIN
             brand_id,
             name,
             description,
-            price_amount,
+            price,
             stock,
             like_count,
             version,
             created_at,
             updated_at
         ) VALUES (
-            FLOOR(1 + RAND() * 10),
+            FLOOR(1 + RAND() * 30),
             CONCAT('Product Name ', i),
-            CONCAT('Description for product ', i),
+            CONCAT('Description ', i),
             FLOOR(1000 + RAND() * 99000),
-            FLOOR(RAND() * 100),
-            FLOOR(RAND() * 5000),
+            FLOOR(RAND() * 1000),
+            FLOOR(RAND() * 500),
             0,
             NOW(),
             NOW()
@@ -52,6 +73,8 @@ BEGIN
     COMMIT;
     SET autocommit = 1;
 END$$
+
 DELIMITER ;
+
 
 CALL loop_insert_products();
