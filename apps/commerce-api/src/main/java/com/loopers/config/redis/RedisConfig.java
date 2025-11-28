28cfe461adb6b893d34f27261f16a8954a882178
yeonjiyeon <dalnim95@naver.com>
2025-11-28 11:06:37 +0900

    chore:redis 관련 설정 추가

---
+++ apps/commerce-api/src/main/java/com/loopers/config/redis/RedisConfig.java
@@ -0,0 +1,61 @@
+package com.loopers.config.redis;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.jsontype.BasicPolymorphicTypeValidator;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.util.List;
+
+@Configuration
+public class RedisConfig {
+
+  @Bean
+  public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+    RedisTemplate<String, Object> template = new RedisTemplate<>();
+    template.setConnectionFactory(connectionFactory);
+
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    objectMapper.registerModule(new JavaTimeModule());
+    objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+    objectMapper.activateDefaultTyping(
+        BasicPolymorphicTypeValidator.builder()
+            .allowIfBaseType(Object.class)
+            .build(),
+        ObjectMapper.DefaultTyping.EVERYTHING
+    );
+
+    objectMapper.addMixIn(PageImpl.class, PageImplMixin.class);
+
+    GenericJackson2JsonRedisSerializer serializer = new GenericJackson2JsonRedisSerializer(objectMapper);
+
+    template.setKeySerializer(new StringRedisSerializer());
+    template.setValueSerializer(serializer);
+    template.setHashKeySerializer(new StringRedisSerializer());
+    template.setHashValueSerializer(serializer);
+
+    return template;
+  }
+
+  @JsonIgnoreProperties(ignoreUnknown = true)
+  abstract static class PageImplMixin<T> {
+    @JsonCreator
+    public PageImplMixin(@JsonProperty("content") List<T> content,
+        @JsonProperty("pageable") JsonNode pageable,
+        @JsonProperty("total") long total) {
+    }
+  }
+}
