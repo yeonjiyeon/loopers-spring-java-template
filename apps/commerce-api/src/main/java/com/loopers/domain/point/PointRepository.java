d321b49deaa3e09f3e69d9b7b98c2d4f790a774c
이정배 <jeongbae1212@gmail.com>
2025-11-28 15:48:32 +0900

    Revert "Round3: Product, Brand, Like, Order"

--- apps/commerce-api/src/main/java/com/loopers/domain/point/PointRepository.java
+++
@@ -1,9 +0,0 @@
-package com.loopers.domain.point;
-
-import java.util.Optional;
-
-public interface PointRepository {
-    Optional<Point> findByUserId(Long userId);
-
-    void save(Point point);
-}
