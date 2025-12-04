88d484f248f98dee6175935ce6c4b08800d6124d
yeonjiyeon <dalnim95@naver.com>
2025-12-04 09:34:11 +0900

    feature: 결제 도메인 및 관련 컴포넌트 추가 (Payment 엔티티, 서비스, 리포지토리 등)

---
+++ apps/commerce-api/src/main/java/com/loopers/infrastructure/payment/PaymentRepositoryImpl.java
@@ -0,0 +1,18 @@
+package com.loopers.infrastructure.payment;
+
+import com.loopers.domain.payment.Payment;
+import com.loopers.domain.payment.PaymentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class PaymentRepositoryImpl implements PaymentRepository {
+
+  private final PaymentJpaRepository paymentJpaRepository;
+
+  @Override
+  public Payment save(Payment payment) {
+    return paymentJpaRepository.save(payment);
+  }
+}
