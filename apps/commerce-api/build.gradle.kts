8c8ffc72c666c08363d8b448f83325060a324eff
yeonjiyeon <dalnim95@naver.com>
2025-12-05 12:36:36 +0900

    chore: 정리된 의존성 재배치 및 Resilience4j 설정 개선 (Retry, CircuitBreaker 세부 조정 포함)

--- apps/commerce-api/build.gradle.kts
+++ apps/commerce-api/build.gradle.kts
@@ -11,6 +11,19 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:${project.properties["springDocOpenApiVersion"]}")
 
+    // Resilience4j (Spring Boot 3.x 기준)
+    implementation("io.github.resilience4j:resilience4j-spring-boot3")
+
+    // AOP
+    implementation("org.springframework.boot:spring-boot-starter-aop")
+
+    //Micrometer Prometheus
+    implementation("io.micrometer:micrometer-registry-prometheus")
+
+    //Spring Cloud OpenFeign
+    implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
+
+
     // querydsl
     annotationProcessor("com.querydsl:querydsl-apt::jakarta")
     annotationProcessor("jakarta.persistence:jakarta.persistence-api")
@@ -19,22 +32,4 @@ dependencies {
     // test-fixtures
     testImplementation(testFixtures(project(":modules:jpa")))
     testImplementation(testFixtures(project(":modules:redis")))
-
-    // Resilience4j (Spring Boot 3.x 기준)
-    implementation("io.github.resilience4j:resilience4j-spring-boot3:2.2.0")
-
-    // AOP
-    implementation("org.springframework.boot:spring-boot-starter-aop")
-
-    // actuator
-    implementation("org.springframework.boot:spring-boot-starter-actuator")
-
-    //Micrometer Prometheus
-    implementation("io.micrometer:micrometer-registry-prometheus")
-
-    //Spring Cloud OpenFeign
-    implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
-
-    //Spring Cloud CircuitBreaker
-    implementation("org.springframework.cloud:spring-cloud-starter-circuitbreaker-resilience4j")
 }
